1bd10643078faf96e35e967d31551e587aaefeaa
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-19 14:55:09 +0530

    props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ const App=()=>{
   return(
     <div>
       <h2>lets get started</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+      date={new Date(2023,7,15)}
+      title="insurance"
+      location="delhi"
+      price="$100"/>
+
+<ExpenseItem
+      date={new Date(2023,7,16)}
+      title="banking"
+      location="hyd"
+      price="$200"/>
+
+<ExpenseItem
+      date={new Date(2023,7,17)}
+      title="business"
+      location="nalgonda"
+      price="$300"/>
+
+<ExpenseItem
+      date={new Date(2023,7,18)}
+      title="market"
+      location="mumbai"
+      price="$500"/>
     </div>
   )
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import "./ExpenseItem.css"
 
-const ExpenseItem=()=>{
+const ExpenseItem=(props)=>{
 
-    const expansedate=new Date(2023,7,15).toISOString();
-    const expansetitle="insurance"
-    const expanseamount="$100"
-    const expanselocation="hyderabad"
+  
     return(
         <div className="expense-item">
-            <div>{expansedate}</div>
-            <div className="expense-item__location">{expanselocation}</div>
-            <div className="expense-item__description"><h2>{expansetitle}</h2></div>
-            <div className="expense-item__price">{expanseamount}</div>
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__location">{props.title}</div>
+            <div className="expense-item__description"><h2>{props.location}</h2></div>
+            <div className="expense-item__price">{props.price}</div>
         </div>
     )
 }
