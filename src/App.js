42e05f9aff892beab65988026f72506865333f94
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-19 11:55:57 +0530

    first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  return(
+    <div>
+      <h2>lets get started</h2>
+      <h2>iam learning react</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
