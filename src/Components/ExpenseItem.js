74de8dea9cd5dd53a05a665ec4c0402ab86142e6
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-19 15:18:36 +0530

    calenderlook

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,22 @@
 import "./ExpenseItem.css"
 
-const ExpenseItem=(props)=>{
+const monthes=[
+    "january","february","march","april","may","june","july","august",
+    "september","octomber","november","december"
+]
 
-  
+const ExpenseItem=(props)=>{
+    const month=monthes[props.date.getMonth()];
+    const year=props.date.getFullYear();
+    const day=props.date.getDate();
+
     return(
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__location">{props.title}</div>
             <div className="expense-item__description"><h2>{props.location}</h2></div>
             <div className="expense-item__price">{props.price}</div>
