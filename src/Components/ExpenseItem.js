2d90bb4f92da8786d783920450c72857dd93594e
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-04-19 14:43:48 +0530

    jsx

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css"
 
 const ExpenseItem=()=>{
+
+    const expansedate=new Date(2023,7,15).toISOString();
+    const expansetitle="insurance"
+    const expanseamount="$100"
+    const expanselocation="hyderabad"
     return(
         <div className="expense-item">
-            <div> 15-august-2023</div>
-            <div className="expense-item__location">delhi</div>
-            <div className="expense-item__description"><h2>book</h2></div>
-            <div className="expense-item__price">$10</div>
+            <div>{expansedate}</div>
+            <div className="expense-item__location">{expanselocation}</div>
+            <div className="expense-item__description"><h2>{expansetitle}</h2></div>
+            <div className="expense-item__price">{expanseamount}</div>
         </div>
     )
 }
